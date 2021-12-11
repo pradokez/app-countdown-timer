bc719c8cc4282f2b6327a6f92d4a2cd771dd3599
pradokez <pradokeziah@gmail.com>
2021-12-11 17:29:30 -0300

    feat: set interval to decrease time

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ function submit() {
     const hoursRemain = (daysRemain - Math.floor(daysRemain)) * 24 + 3;
 
     const minutesRemain = (hoursRemain - Math.floor(hoursRemain)) * 60;
-    
+
     const secondsRemain = (minutesRemain - Math.floor(minutesRemain)) * 60;
 
-
-    console.log(today);
-    console.log(`${Math.floor(daysRemain)} dias restantes`);
-    console.log(`${Math.floor(hoursRemain)} horas restantes`);
-    console.log(`${Math.floor(minutesRemain)} minutos restantes`);
-    console.log(`${Math.floor(secondsRemain)} segundos restantes`);
     
+    document.getElementById("days").innerHTML = Math.floor(daysRemain);
+    document.getElementById("hours").innerHTML = Math.floor(hoursRemain);
+    document.getElementById("minutes").innerHTML = Math.floor(minutesRemain);
+    document.getElementById("seconds").innerHTML = Math.floor(secondsRemain);
   }
+
+  setInterval('submit()', 1000);
 }
